2e7464ec3fd1e970f07fa3e28359818f4fe60127
estasney <estasney@users.noreply.github.com>
2020-12-28 15:22:15 -0500

    Use Intervals for models.py

--- reolink/models.py
+++ reolink/models.py
@@ -1,12 +1,10 @@
 from datetime import datetime
-from typing import Optional
-
-from sqlalchemy import Column, Integer, Boolean, DateTime, String, ForeignKey
+from psycopg2.extras import DateTimeRange
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.dialects.postgresql import TSRANGE
 
 Base = declarative_base()
 Session = sessionmaker()
@@ -20,14 +18,13 @@ def get_session(db_url) -> Session:
     return session
 
 
-class MotionDetection(Base):
-    __tablename__ = 'motions'
+class MotionRange(Base):
+    __tablename__ = 'motionranges'
 
     id = Column(Integer, primary_key=True)
     channel_id = Column(Integer, ForeignKey('channels.id'))
-    channel = relationship("Channel", back_populates="detections")
-    detected = Column(Boolean)
-    dt = Column(DateTime, default=datetime.now())
+    channel = relationship("Channel", back_populates="intervals")
+    range = Column(TSRANGE())
 
 
 class Channel(Base):
@@ -35,11 +32,51 @@ class Channel(Base):
 
     id = Column(Integer, primary_key=True, autoincrement=False)
     name = Column(String(128))
-    detections = relationship("MotionDetection", back_populates="channel", order_by="desc(MotionDetection.dt)")
+    motion_started = Column(DateTime, nullable=True, default=None)
+    motions = relationship("MotionRange", back_popuates="channel")
 
-    @hybrid_property
-    def last_state(self) -> Optional[bool]:
-        detect = self.detections
-        if detect:
-            return detect[0].detected
-        return None
+    def handle_detection(self, detection: bool, session: Session, force_new: bool = False):
+        """
+        Passed a boolean, handles tracking state with MotionInterval.
+
+        Parameters
+        ----------
+        detection : bool
+            Boolean if motion is detected
+        session : Session
+            session connection
+        force_new : bool
+            If True, `self.motion_started` is set to `None`
+
+        Returns
+        -------
+        changed : bool
+            If True, session should be committed to track changes
+
+        """
+        changed = False
+
+        if force_new:
+            if self.motion_started:
+                changed = True
+            self.motion_started = None
+
+        if self.motion_started is None and detection:
+            self.motion_started = datetime.now()
+            changed = True
+            return changed
+        elif self.motion_started is None and not detection:
+            return changed
+        elif self.motion_started is not None and detection:
+            return changed
+        elif self.motion_started is not None and not detection:
+
+            md_interval = MotionRange(channel_id=self.id,
+                                      range=DateTimeRange(self.motion_started, datetime.now()))
+            session.add(md_interval)
+            self.motion_started = None
+            changed = True
+            return changed
+        else:
+            raise Exception("Logic Error")
+
