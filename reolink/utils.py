763afeda67c2917d3518ce2d55da649dbc3bb24f
estasney <estasney@users.noreply.github.com>
2020-12-29 00:07:12 -0500

    add missing month parameter. Improve readability

--- reolink/utils.py
+++ reolink/utils.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from datetime import datetime
 from typing import List
 from toolz.dicttoolz import get_in
 
@@ -38,6 +39,15 @@ class SearchResultFile:
     type: str
     width: int
 
+    def __repr__(self):
+        st = self.StartTime.dt.strftime("%m/%d %I:%M:%S %p")
+        pt = self.PlaybackTime.dt.strftime("%m/%d %I:%M:%S %p")
+        et = self.EndTime.dt.strftime("%m/%d %I:%M:%S %p")
+
+        return f"{st} :: {pt} :: {et}"
+
+
+
     @classmethod
     def _from_file_resp(cls, file: dict):
         init_data = {
@@ -54,16 +64,29 @@ class SearchResultFile:
 
 @dataclass
 class SearchResultTime:
-    day: int
-    hour: int
-    min: int
-    sec: int
-    year: int
+    _day: int
+    _hour: int
+    _min: int
+    _sec: int
+    _mon: int
+    _year: int
+    dt: datetime = field(init=False)
+
+    def __repr__(self):
+        return self.dt.__repr__()
+
+    def __str__(self):
+        return self.dt.__str__()
 
     @classmethod
     def from_dict(cls, time: dict):
-        init_data = {k: time[k] for k in ["day", "hour", "min", "sec", "year"]}
+        init_data = {k: time[k[1:]] for k in ["_day", "_hour", "_min", "_sec", "_mon", "_year"]}
         return SearchResultTime(**init_data)
+
+    def __post_init__(self):
+        self.dt = datetime(self._year, self._mon, self._day, self._hour, self._min, self._sec)
+
+
 
 
 
