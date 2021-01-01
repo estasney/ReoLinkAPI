4b6515fdea032db8967e967867fa71c5c608dab6
estasney <estasney@users.noreply.github.com>
2021-01-01 15:37:20 -0500

    wip

--- reolink/runners/fetch.py
+++ reolink/runners/fetch.py
@@ -1,16 +1,18 @@
 import asyncio
 import logging.config
+import math
+import os
+import subprocess
 from datetime import datetime
-from typing import Optional, cast, List
+from typing import Optional, cast, List, Tuple
 from urllib.parse import urlparse, urlencode
-
+from tempfile import TemporaryDirectory
 from dateutil.relativedelta import relativedelta
 
 from reolink.camera_api import Api, STREAM_TYPES
 from reolink.utils import SearchResponse, SearchResultFile, dt_string
 
 logger = logging.getLogger('fetch')
-# logging.config.fileConfig('logging.conf')
 
 
 def get_stream(api: Api, start_time: datetime, port: int = 1935, channel: Optional[int] = None,
@@ -44,33 +46,22 @@ def get_stream(api: Api, start_time: datetime, port: int = 1935, channel: Option
     # Determine a window for querying files that meet specified start_time
     window_start = start_time - relativedelta(hours=4)
     window_end = start_time + relativedelta(hours=4)
-    window_end = min(datetime.now(), window_end)  # Future dates will cause error
 
-    recordings = asyncio.run(api.query_recordings(window_start, window_end, channel=channel, stream=stream))
+    # Future dates will cause error
+    # It also seems to throw an error when querying currently recording
+    window_end = min((datetime.now() - relativedelta(hours=1)), window_end)
 
-    if not recordings:
-        raise Exception(f"No Recordings Match Found for {start_time}")
-
-    responses = cast(List[SearchResponse], recordings)
-    rec_files = [file for rfiles in [r.files for r in responses] for file in rfiles]  # Flatten
-
-    matched_file = next((f for f in rec_files if f.StartTime.dt <= start_time < f.EndTime.dt), None)
-
-    if not matched_file:
-        raise Exception(f"No Recordings Match Found for {start_time}")
-
-    seek_time = int(round((start_time - matched_file.StartTime.dt).total_seconds(), 0))
-    matched_file_start_dt_str = dt_string(matched_file.PlaybackTime.dt)
+    matched_file_start_dt_str, seek_time = url_logic_start_time(api, channel, start_time, stream)
 
     params = {
-        "port": port,
-        "app": "bcs",
-        "stream": "playback.bcs",
+        "port":    port,
+        "app":     "bcs",
+        "stream":  "playback.bcs",
         "channel": channel if channel else api.channel,
-        "type": 1,
-        "start": matched_file_start_dt_str,
-        "seek": seek_time,
-        "token": api.token
+        "type":    1,
+        "start":   matched_file_start_dt_str,
+        "seek":    seek_time,
+        "token":   api.token
         }
 
     url_params = urlencode(params)
@@ -81,14 +72,189 @@ def get_stream(api: Api, start_time: datetime, port: int = 1935, channel: Option
     return url_stream
 
 
+def url_logic_start_time(api, channel, start_time, stream) -> Tuple[str, int]:
+    """
+    Get the filename and seektime that matches a start_time
+
+    Parameters
+    ----------
+    api
+    channel
+    start_time
+    stream
+
+    Returns
+    -------
+
+    """
+    # Determine a window for querying files that meet specified start_time
+    recordings = query_window(api, channel, start_time, stream)
+    if not recordings:
+        raise Exception(f"No Recordings Match Found for {start_time}")
+    responses = cast(List[SearchResponse], recordings)
+    rec_files = [file for rfiles in [r.files for r in responses] for file in rfiles]  # Flatten
+    matched_file = next((f for f in rec_files if start_time in f), None)
+    if not matched_file:
+        raise Exception(f"No Recordings Match Found for {start_time}")
+    seek_time = math.floor((start_time - matched_file.StartTime.dt).total_seconds())
+    matched_file_start_dt_str = dt_string(matched_file.PlaybackTime.dt)
+    return matched_file_start_dt_str, seek_time
+
+
+def query_window(api, channel, start_time, stream):
+    """
+    Run a query with window
+
+    Parameters
+    ----------
+    api
+    channel
+    start_time
+    stream
+
+    Returns
+    -------
+
+    """
+    window_start = start_time - relativedelta(hours=4)
+    window_end = start_time + relativedelta(hours=4)
+    # Future dates will cause error
+    # It also seems to throw an error when querying currently recording
+    window_end = min((datetime.now() - relativedelta(hours=1)), window_end)
+    recordings = asyncio.run(api.query_recordings(window_start, window_end, channel=channel, stream=stream))
+    return recordings
+
+
+def url_logic_duration(api, channel, start_time, duration_secs, stream):
+    """
+    Get the filename(s) and seektimes that matches a start_time and duration
+
+    Parameters
+    ----------
+    api
+    channel
+    start_time
+    duration_secs
+    stream
+
+    Returns
+    -------
+
+    """
+
+    recordings = query_window(api, channel, start_time, stream)
+    if not recordings:
+        raise Exception(f"No Recordings Match Found for {start_time}")
+    responses = cast(List[SearchResponse], recordings)
+    rec_files = [file for rfiles in [r.files for r in responses] for file in rfiles]  # Flatten
+    rec_files.sort(key=lambda x: x.StartTime.dt)
 
 
 
+def save_stream(api: Api, start_time: datetime, duration_secs: int, fp: str, padding_secs: int = 5, port: int = 1935,
+                channel: Optional[int] = None,
+                stream: Optional[STREAM_TYPES] = 'sub'):
+    """
+    Saves a stream with FFMpeg
 
 
+    Parameters
+    ----------
+    api
+    start_time
+    duration_secs
+    fp
+    padding_secs : int
+        Number of seconds
+    port
+    channel
+    stream
+    fp
+
+    Returns
+    -------
+    """
+
+    # We also need to handle cases where durations may cause files to be split
+    _, rec_files = get_stream(api=api, start_time=start_time, stream=stream)
+
+    # True start time adjusted for padding
+    pad_start_time = start_time - relativedelta(seconds=padding_secs)
+    end_time = start_time + relativedelta(seconds=duration_secs)
+
+    rec_files = sorted(rec_files, key=lambda x: x.StartTime.dt)
+
+    duration_timer = pad_start_time
+    duration_remain = duration_secs
+
+    tasks = []
+
+    for file in rec_files:
+        # Seconds that can conribute to requested
+        if file.EndTime.dt < pad_start_time:
+            continue
+        if file.StartTime.dt > end_time:
+            continue
+        useable_seconds = math.floor((file.EndTime.dt - pad_start_time).total_seconds())
+        if useable_seconds >= duration_remain:
+            tasks.append((file, duration_timer, duration_remain))
+            break
+        else:
+            tasks.append((file, duration_timer, useable_seconds))
+            duration_timer += relativedelta(seconds=useable_seconds)
+            duration_remain -= useable_seconds
 
 
+    logger.info(f"Writing {len(tasks)} Video Files")
 
+    # store in tempdir
+    merge_dir = TemporaryDirectory()
+    merge_fp = merge_dir.name
+
+    for i, (file, dtimer, secs) in enumerate(tasks):
+        seek_time = int(round((dtimer - file.StartTime.dt).total_seconds(), 0))
+        stream_url_start_dt = dt_string(file.PlaybackTime.dt)
+        params = {
+            "port":    port,
+            "app":     "bcs",
+            "stream":  "playback.bcs",
+            "channel": channel if channel else api.channel,
+            "type":    1,
+            "start":   stream_url_start_dt,
+            "seek":    seek_time,
+            "token":   api.token
+            }
+
+        url_params = urlencode(params)
+        url_stream = f"http://{api.host}/flv?{url_params}"
+        file_name = f"{i}.mp4"
+        full_path = os.path.join(merge_fp, file_name)
+
+
+        with subprocess.Popen(['ffmpeg', "-to", str(secs), "-i", url_stream, full_path],
+                              stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+                              shell=False) as p:
+            try:
+                result, _ = p.communicate(timeout=secs + 30)
+            except:
+                p.terminate()
+        print(result.decode().strip())
+
+    # rejoin videos
+
+    vidlist_fp = os.path.join(merge_fp, "vidlist.txt")
+    with open(vidlist_fp, "w") as vfp:
+        for i in range(len(tasks)):
+            vfp.write(f"{i}.mp4")
+            vfp.write("\n")
+
+    with subprocess.Popen(['ffmpeg', '-f', 'concat', '-safe', '0', '-i', vidlist_fp, '-c', 'copy', fp],
+                          stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False) as p:
+        result, _ = p.communicate()
+
+    print(result.decode().strip())
+
+    merge_dir.cleanup()
 
 
 
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,5 +1,5 @@
 from reolink.camera_api import Api
-from reolink.runners.fetch import get_stream
+from reolink.runners.fetch import get_stream, save_stream
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import keyring
@@ -7,14 +7,28 @@ import asyncio
 import subprocess
 
 
-def test_fetch(offset_hours):
+def test_fetch(offset_hours, padding_sec=5):
     api = Api("10.1.0.120", "dev", keyring.get_password("REOLINK", "dev"), stream='sub')
     asyncio.run(api.login())
     s = datetime.now() - relativedelta(hours=offset_hours)
-    url = get_stream(api, s, stream='sub')
-
     print(f"Getting Playback at {s.strftime('%m/%d %I:%M:%S %p')}")
+
+    s = s - relativedelta(seconds=padding_sec)
+    url, _ = get_stream(api, s, stream='sub')
+
 
     with subprocess.Popen(['ffplay', url], stdout=subprocess.PIPE, stdin=subprocess.PIPE, shell=False) as p:
         result, _ = p.communicate()
     print(result.decode().strip())
+
+def test_offset_fetch(offset_hours, padding_sec=5):
+    api = Api("10.1.0.120", "dev", keyring.get_password("REOLINK", "dev"), stream='sub')
+    asyncio.run(api.login())
+    s = datetime.now() - relativedelta(hours=offset_hours)
+    s = datetime(year=s.year, month=s.month, day=s.day, hour=s.hour, minute=59, second=0)
+    print(f"Getting Playback at {s.strftime('%m/%d %I:%M:%S %p')}")
+
+    s = s - relativedelta(seconds=padding_sec)
+    tasks = save_stream(api, s, stream='sub', fp='/home/eric/Downloads/testoffset.mp4', duration_secs=600)
+
+    print(tasks)
